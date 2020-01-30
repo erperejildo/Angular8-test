fe8b3dd408d6e175af948234d6c851bbb19c99fb
Daniel <d15_1_89@msn.com>
2020-01-31 00:58:02 +0100

    Sctructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { BottomContainerComponent } from './bottom-container/bottom-container.component';
+import { AdvertisementComponent } from './advertisement/advertisement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    BottomContainerComponent,
+    AdvertisementComponent
   ],
   imports: [
     BrowserModule,
