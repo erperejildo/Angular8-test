863a05d9734239ee611664859dfed55bdc48bcbe
Daniel <d15_1_89@msn.com>
2020-01-31 11:05:29 +0100

    STar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { BottomContainerComponent } from './bottom-container/bottom-container.component';
 import { AdvertisementComponent } from './advertisement/advertisement.component';
+import { StarRatingComponent } from './star-rating/star-rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
     BottomContainerComponent,
-    AdvertisementComponent
+    AdvertisementComponent,
+    StarRatingComponent
   ],
   imports: [
     BrowserModule,
